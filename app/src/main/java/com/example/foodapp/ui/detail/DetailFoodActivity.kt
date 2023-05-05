1b5fff1a5ab79acb0dda7929e953649e3ab93e07
hutamasatrio <hutamasatrio@gmail.com>
2023-05-06 03:58:03 +0700

    function done

--- app/src/main/java/com/example/foodapp/ui/detail/DetailFoodActivity.kt
+++ app/src/main/java/com/example/foodapp/ui/detail/DetailFoodActivity.kt
@@ -1,12 +1,16 @@
 package com.example.foodapp.ui.detail
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.bumptech.glide.Glide
 import com.example.core.domain.model.Food
 import com.example.core.domain.model.FoodDetail
 import com.example.core.source.db.remote.Resource
 import com.example.foodapp.databinding.ActivityDetailFoodBinding
+import kotlinx.android.synthetic.main.activity_detail_food.*
 import org.koin.android.ext.android.inject
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
@@ -52,7 +56,167 @@ class DetailFoodActivity : AppCompatActivity() {
     }
 
     private fun tesData(foodDetail: FoodDetail?) {
-        Toast.makeText(this,foodDetail?.strMeal,Toast.LENGTH_SHORT).show()
+//        collapsing_toolbar.title = foodDetail?.strMeal.toString()
+        Glide.with(this@DetailFoodActivity)
+            .load(foodDetail?.strMealThumb)
+            .into(mealThumb)
+        collapsing_toolbar.title = foodDetail?.strMeal
+        category.text = foodDetail?.strCategory
+        country.text = foodDetail?.strArea
+        instructions.text = foodDetail?.strInstructions
+//        dataMeal = foodDetail?
+
+        for (i  in 0..21 ){
+            if (foodDetail?.strIngredient1?.isNotEmpty() == true) {
+                ingredient.append("\n \u2022 " + foodDetail?.strIngredient1)
+            }
+        }
+
+        if (foodDetail?.strIngredient1?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient1)
+        }
+        if (foodDetail?.strIngredient2?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient2)
+        }
+        if (foodDetail?.strIngredient3?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient3)
+        }
+        if (foodDetail?.strIngredient4?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient4)
+        }
+        if (foodDetail?.strIngredient5?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient5)
+        }
+        if (foodDetail?.strIngredient6?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient6)
+        }
+        if (foodDetail?.strIngredient7?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient7)
+        }
+        if (foodDetail?.strIngredient8?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient8)
+        }
+        if (foodDetail?.strIngredient9?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient9)
+        }
+        if (foodDetail?.strIngredient10?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient10)
+        }
+        if (foodDetail?.strIngredient11?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient11)
+        }
+        if (foodDetail?.strIngredient12?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient12)
+        }
+        if (foodDetail?.strIngredient13?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient13)
+        }
+        if (foodDetail?.strIngredient14?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient14)
+        }
+        if (foodDetail?.strIngredient15?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient15)
+        }
+        if (foodDetail?.strIngredient16?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient16)
+        }
+        if (foodDetail?.strIngredient17?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient17)
+        }
+        if (foodDetail?.strIngredient18?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient18)
+        }
+        if (foodDetail?.strIngredient19?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient19)
+        }
+        if (foodDetail?.strIngredient20?.isNotEmpty() == true) {
+            ingredient.append("\n \u2022 " + foodDetail?.strIngredient20)
+        }
+
+        if (foodDetail?.strMeasure1?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure1!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure1)
+        }
+        if (foodDetail?.strMeasure2?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure2!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure2)
+        }
+        if (foodDetail?.strMeasure3?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure3!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure3)
+        }
+        if (foodDetail?.strMeasure4?.isNotEmpty() == true && !Character.isWhitespace(
+                foodDetail?.strMeasure4?.get(
+                    0
+                )!!
+            )
+        ) {
+            measure.append("\n : " + foodDetail?.strMeasure4)
+        }
+        if (foodDetail?.strMeasure5?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure5!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure5)
+        }
+        if (foodDetail?.strMeasure6?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure6!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure6)
+        }
+        if (foodDetail?.strMeasure7?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure7!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure7)
+        }
+        if (foodDetail?.strMeasure8?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure8!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure8)
+        }
+        if (foodDetail?.strMeasure9?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure9!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure9)
+        }
+        if (foodDetail?.strMeasure10?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure10!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure10)
+        }
+        if (foodDetail?.strMeasure11?.isNotEmpty() == true && !Character.isWhitespace(
+                foodDetail?.strMeasure11?.get(
+                    0
+                )!!
+            )
+        ) {
+            measure.append("\n : " + foodDetail?.strMeasure11)
+        }
+        if (foodDetail?.strMeasure12?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure12!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure12)
+        }
+        if (foodDetail?.strMeasure13?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure13!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure13)
+        }
+        if (foodDetail?.strMeasure14?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure14!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure14)
+        }
+        if (foodDetail?.strMeasure15?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure15!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure15)
+        }
+        if (foodDetail?.strMeasure16?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure16!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure16)
+        }
+        if (foodDetail?.strMeasure17?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure17!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure17)
+        }
+        if (foodDetail?.strMeasure18?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure18!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure18)
+        }
+        if (foodDetail?.strMeasure19?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure19!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure19)
+        }
+        if (foodDetail?.strMeasure20?.isNotEmpty() == true && !Character.isWhitespace(foodDetail?.strMeasure20!![0])) {
+            measure.append("\n : " + foodDetail?.strMeasure20)
+        }
+
+        youtube.setOnClickListener {
+            val intentYoutube = Intent(Intent.ACTION_VIEW)
+            intentYoutube.data = Uri.parse(foodDetail?.strYoutube)
+            startActivity(intentYoutube)
+        }
+
+        source.setOnClickListener {
+            val intentSource = Intent(Intent.ACTION_VIEW)
+            intentSource.data = Uri.parse(foodDetail?.strSource)
+            startActivity(intentSource)
+        }
+
+        Toast.makeText(this,foodDetail?.strMeal.toString(),Toast.LENGTH_SHORT).show()
     }
 
 }
--- core/src/main/java/com/example/core/source/db/remote/response/FoodDetailItemResponse.kt
+++ core/src/main/java/com/example/core/source/db/remote/response/FoodDetailItemResponse.kt
@@ -4,162 +4,154 @@ import com.google.gson.annotations.SerializedName
 
 data class FoodDetailItemResponse(
 
-	@field:SerializedName("strImageSource")
-	val strImageSource: String? = null,
-
 	@field:SerializedName("strIngredient10")
-	val strIngredient10: String? = null,
+	val strIngredient10: String,
 
 	@field:SerializedName("strIngredient12")
-	val strIngredient12: String? = null,
+	val strIngredient12: String,
 
 	@field:SerializedName("strIngredient11")
-	val strIngredient11: String? = null,
+	val strIngredient11: String,
 
 	@field:SerializedName("strIngredient14")
-	val strIngredient14: String? = null,
+	val strIngredient14: String,
 
 	@field:SerializedName("strCategory")
-	val strCategory: String? = null,
+	val strCategory: String,
 
 	@field:SerializedName("strIngredient13")
-	val strIngredient13: String? = null,
+	val strIngredient13: String,
 
 	@field:SerializedName("strIngredient16")
-	val strIngredient16: String? = null,
+	val strIngredient16: String,
 
 	@field:SerializedName("strIngredient15")
-	val strIngredient15: String? = null,
+	val strIngredient15: String,
 
 	@field:SerializedName("strIngredient18")
-	val strIngredient18: String? = null,
+	val strIngredient18: String,
 
 	@field:SerializedName("strIngredient17")
-	val strIngredient17: String? = null,
+	val strIngredient17: String,
 
 	@field:SerializedName("strArea")
-	val strArea: String? = null,
-
-	@field:SerializedName("strCreativeCommonsConfirmed")
-	val strCreativeCommonsConfirmed: String? = null,
+	val strArea: String,
 
 	@field:SerializedName("strIngredient19")
-	val strIngredient19: String? = null,
+	val strIngredient19: String,
 
 	@field:SerializedName("strTags")
-	val strTags: String? = null,
+	val strTags: String,
 
 	@field:SerializedName("idMeal")
-	val idMeal: String? = null,
+	val idMeal: String,
 
 	@field:SerializedName("strInstructions")
-	val strInstructions: String? = null,
+	val strInstructions: String,
 
 	@field:SerializedName("strIngredient1")
-	val strIngredient1: String? = null,
+	val strIngredient1: String,
 
 	@field:SerializedName("strIngredient3")
-	val strIngredient3: String? = null,
+	val strIngredient3: String,
 
 	@field:SerializedName("strIngredient2")
-	val strIngredient2: String? = null,
+	val strIngredient2: String,
 
 	@field:SerializedName("strIngredient20")
-	val strIngredient20: String? = null,
+	val strIngredient20: String,
 
 	@field:SerializedName("strIngredient5")
-	val strIngredient5: String? = null,
+	val strIngredient5: String,
 
 	@field:SerializedName("strIngredient4")
-	val strIngredient4: String? = null,
+	val strIngredient4: String,
 
 	@field:SerializedName("strIngredient7")
-	val strIngredient7: String? = null,
+	val strIngredient7: String,
 
 	@field:SerializedName("strIngredient6")
-	val strIngredient6: String? = null,
+	val strIngredient6: String,
 
 	@field:SerializedName("strIngredient9")
-	val strIngredient9: String? = null,
+	val strIngredient9: String,
 
 	@field:SerializedName("strIngredient8")
-	val strIngredient8: String? = null,
+	val strIngredient8: String,
 
 	@field:SerializedName("strMealThumb")
-	val strMealThumb: String? = null,
+	val strMealThumb: String,
 
 	@field:SerializedName("strMeasure20")
-	val strMeasure20: String? = null,
+	val strMeasure20: String,
 
 	@field:SerializedName("strYoutube")
-	val strYoutube: String? = null,
+	val strYoutube: String,
 
 	@field:SerializedName("strMeal")
-	val strMeal: String? = null,
+	val strMeal: String,
 
 	@field:SerializedName("strMeasure12")
-	val strMeasure12: String? = null,
+	val strMeasure12: String,
 
 	@field:SerializedName("strMeasure13")
-	val strMeasure13: String? = null,
+	val strMeasure13: String,
 
 	@field:SerializedName("strMeasure10")
-	val strMeasure10: String? = null,
+	val strMeasure10: String,
 
 	@field:SerializedName("strMeasure11")
-	val strMeasure11: String? = null,
-
-	@field:SerializedName("dateModified")
-	val dateModified: String? = null,
-
-	@field:SerializedName("strDrinkAlternate")
-	val strDrinkAlternate: String? = null,
+	val strMeasure11: String,
 
 	@field:SerializedName("strSource")
-	val strSource: String? = null,
+	val strSource: String,
 
 	@field:SerializedName("strMeasure9")
-	val strMeasure9: String? = null,
+	val strMeasure9: String,
 
 	@field:SerializedName("strMeasure7")
-	val strMeasure7: String? = null,
+	val strMeasure7: String,
 
 	@field:SerializedName("strMeasure8")
-	val strMeasure8: String? = null,
+	val strMeasure8: String,
 
 	@field:SerializedName("strMeasure5")
-	val strMeasure5: String? = null,
+	val strMeasure5: String,
 
 	@field:SerializedName("strMeasure6")
-	val strMeasure6: String? = null,
+	val strMeasure6: String,
 
 	@field:SerializedName("strMeasure3")
-	val strMeasure3: String? = null,
+	val strMeasure3: String,
 
 	@field:SerializedName("strMeasure4")
-	val strMeasure4: String? = null,
+	val strMeasure4: String,
 
 	@field:SerializedName("strMeasure1")
-	val strMeasure1: String? = null,
+	val strMeasure1: String,
 
 	@field:SerializedName("strMeasure18")
-	val strMeasure18: String? = null,
+	val strMeasure18: String,
 
 	@field:SerializedName("strMeasure2")
-	val strMeasure2: String? = null,
+	val strMeasure2: String,
 
 	@field:SerializedName("strMeasure19")
-	val strMeasure19: String? = null,
+	val strMeasure19: String,
 
 	@field:SerializedName("strMeasure16")
-	val strMeasure16: String? = null,
+	val strMeasure16: String,
 
 	@field:SerializedName("strMeasure17")
-	val strMeasure17: String? = null,
+	val strMeasure17: String,
 
 	@field:SerializedName("strMeasure14")
-	val strMeasure14: String? = null,
+	val strMeasure14: String,
 
 	@field:SerializedName("strMeasure15")
-	val strMeasure15: String? = null
+	val strMeasure15: String
+
+
+
+
 )
